0fc6b03f68907b90e7dd19735f6b112a0bf74d01
Marcin Noga <marcin@careaboutit.pl>
2021-11-17 10:44:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+[![N|FC](https://futurecollars.com/wp-content/uploads/2019/09/logo.svg)](https://futurecollars.com/)
+
+# JS - Projekt 2
+
+## Przelicznik walut
+
+Korzystając z darmowego API ze strony NBP (https://api.nbp.pl/?ref=public-apis) należy stworzyć prosty przelicznik walut uwzględniający następujące waluty: euro, dolary amerykańskie, franki szwajcarskie. Aplikacja ma umożliwiać przeliczanie wybranej waluty na złotówki. Kurs danej waluty ma być brany jako najnowszy (z dnia bieżącego).
+
+Tak wygląda przykładowy wygląd interfejsu:
+
+Stylowanie aplikacji dowolne. Dodatkowe punkty za dołożenie loadera w momencie pobierania danych z NBP. Projekt musi być umieszczony na Githubie lub Gitlabie.
---
+++ js/app.js
@@ -0,0 +1,89 @@
+const apiCurrencies = axios.create({
+  baseURL: "http://api.nbp.pl/api/exchangerates/rates/a",
+  onDownloadProgress: (progressEvent) => {
+    let percentCompleted = Math.round(
+      (progressEvent.loaded * 100) / progressEvent.total
+    );
+    console.log(percentCompleted);
+  },
+});
+
+const init = () => {
+  formSend();
+};
+
+const formSend = () => {
+  const form = document.querySelector(".js-calculate");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    makeProgress();
+
+    const plnValueToConvert = document.getElementById("amountInput");
+    const selectedCurrency = document.getElementById("currenciesList");
+
+    if (plnValueToConvert.value) {
+      const data = await getCurrencyValue(selectedCurrency.value);
+      const result = document.getElementById("result");
+
+      const calculatedValue = convertToPLN(
+        plnValueToConvert.value,
+        data.rates[0].mid
+      );
+
+      if (calculatedValue >= 0) {
+        result.innerHTML = `${calculatedValue} PLN`;
+      } else {
+        result.innerHTML = `0 PLN`;
+      }
+    }
+    return false;
+  });
+};
+
+const convertToPLN = (plnValue, rate) => {
+  if (plnValue < 0) {
+    alert("Podaj wartość większą od zera!");
+    return -1;
+  }
+  return (plnValue * rate).toFixed(2);
+};
+
+const getCurrencyValue = async (currency) => {
+  if (currency) {
+    const response = await apiCurrencies(`/${currency}/`).catch((error) => {
+      console.log("Error");
+      return false;
+    });
+
+    if (response) {
+      const { data } = response;
+      return data;
+    }
+  } else {
+    return false;
+  }
+};
+
+if (document.readyState !== "loading") {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init());
+}
+
+//progress bar
+
+function makeProgress() {
+  const bar = document.querySelector(".progress-bar");
+
+  bar.style.transition = "none";
+  bar.style.width = 0;
+  bar.innerText = 0;
+
+  for (let i = 0; i <= 100; i = i + 1) {
+    setTimeout(function timer() {
+      bar.style.width = i + "%";
+      bar.innerText = i + "%";
+    }, 200);
+  }
+}
